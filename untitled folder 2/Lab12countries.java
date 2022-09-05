fcacfec3b380165ca4a86ac2bd9dae8a3fc935ef
64512 <abdullahkhan@cpp.edu>
2022-09-05 14:31:52 -0700

    finalcommit

--- untitled folder 2/Lab12countries.java
+++ untitled folder 2/Lab12countries.java
@@ -2,7 +2,7 @@ package lab12countries;
 public class Lab12countries {
     public static void main(String[] args) {
         String name[] = {"India", "Pakistan","United States","Brazil","Nigeria","Russia","Indonesia","Mexico","Japan","Bangladesh","Ethiopia","China"};
-        int number[] = {128,228,354,383,554,667,789,825,812,103,114,127};
+        int number[] = {128,228,354,383,554,665,789,825,812,103,114,127};
         for (int i=0;i<name.length;i++){
             System.out.println(name[i]+ " " +number[i] );
         }
